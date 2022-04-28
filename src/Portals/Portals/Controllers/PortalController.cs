791e640774fe8dc5775f69dffd8f68518827fea3
Park YongJune <redplus@redplus.net>
2022-04-29 01:54:06 +0900

    MVC 컨트롤러와 뷰 만들고 Blazor 컴포넌트를 component 태그 헬퍼로 포함하여 실행하기

---
+++ src/Portals/Portals/Controllers/PortalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portals.Controllers
+{
+    public class PortalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
