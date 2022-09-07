b5af991624d9741710b9bc4bf32bfe0e6fbd1aba
Park YongJune <redplus@redplus.net>
2022-09-08 03:51:47 +0900

    .

---
+++ src/Portals/Portals/Areas/Identity/Services/EmailSender.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace Hawaso.Areas.Identity.Services
+{
+    // Abstractions: Interfaces => IEmailSender
+    // Implementations: Classes => EmailSender, SendGridEmailSender, ...
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
